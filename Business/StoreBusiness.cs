16aa8a7c21a491fd1ef0a6e75f0db7a39915c233
benjasaldao <saldaobenjamin@gmail.com>
2024-06-17 22:25:55 -0300

    Finished storeBusiness, class storeUser modified

---
+++ Business/StoreBusiness.cs
@@ -0,0 +1,160 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class StoreBusiness
+    {
+        private DataAccess data;
+
+        public StoreBusiness() {
+            data = new DataAccess();
+        }
+
+        /// <summary>
+        /// Creates a new store register in the database
+        /// </summary>
+        /// <param name="store"></param>
+        /// <param name="user"></param>
+        /// <returns>TRUE if it was created sucessfully</returns>
+        public bool create(Store store, User user)
+        {
+            try
+            {
+                data.setProcedure("spCreateStore");
+                data.setParam("@name", store.name);
+                data.setParam("@description", store.description);
+                data.setParam("@userId", user.id);
+                data.executeAction();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            } finally
+            {
+                data.closeConnection();
+            }
+        }
+
+
+        /// <summary>
+        /// Creates a new store register in the database
+        /// </summary>
+        /// <param name="store"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool create(Store store, int id)
+        {
+            try
+            {
+                data.setProcedure("spCreateStore");
+                data.setParam("@name", store.name);
+                data.setParam("@description", store.description);
+                data.setParam("@userId", id);
+                data.executeAction();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                data.closeConnection();
+            }
+        }
+
+        /// <summary>
+        /// List all the stores asociated to a user
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>a list of stores</returns>
+        public List<Store> listByUser(int userId)
+        {
+            try
+            {
+                List<Store> list = new List<Store>();
+
+                data.setProcedure("spListStoresByUser");
+                data.setParam("@userId", userId);
+                data.executeAction();
+
+                while (data.Reader.Read())
+                {
+                    Store store = new Store();
+
+                    store.id = (int)data.Reader["id"];
+                    if (!(data.Reader["description"] is DBNull))
+                        store.description = (String)data.Reader["description"];
+                    if (!(data.Reader["name"] is DBNull))
+                        store.name = (String)data.Reader["name"];
+                    store.userId = (int)data.Reader["userId"];
+                    store.state = (bool)data.Reader["state"];
+
+                    list.Add(store);
+                }
+            
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            } finally { data.closeConnection(); }
+        }
+
+        /// <summary>
+        /// updates a store register
+        /// </summary>
+        /// <param name="store"></param>
+        /// <returns>TRUE if it was successfully updated</returns>
+        public bool update(Store store)
+        {
+            try
+            {
+                data.setProcedure("spUpdateStore");
+                data.setParam("@name", store.name);
+                data.setParam("@description", store.description);
+                data.setParam("@id", store.id);
+              
+                data.executeAction();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { data.closeConnection(); }
+        }
+
+
+        /// <summary>
+        /// Deletes a given store register in the database
+        /// </summary>
+        /// <param name="store"></param>
+        /// <returns>TRUE if it was deleted successfully</returns>
+        public bool delete(Store store)
+        {
+            try
+            {
+                data.setProcedure("spDeleteStore");
+                data.setParam("@id", store.id);
+                data.executeAction();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { data.closeConnection(); }
+        }
+
+    }
+}
--- Domain/StoreUser.cs
+++ Domain/StoreUser.cs
@@ -6,14 +6,11 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class StoreUser
+    /// <summary>
+    /// User that is created in a created store
+    /// </summary>
+    public class StoreUser : User
     {
-        public int id { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string name { get; set; }
-        public string surname { get; set; }
-        public string recoveryToken { get; set; }
         public string adress { get; set; }
         public string city { get; set; }
         public int storeId { get; set; }
